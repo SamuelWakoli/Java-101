7dc06ae9155d1f2df33ab4f180e43d826b9fc9f1
SamuelWakoli <swwakoli@gmail.com>
2023-03-21 01:05:41 +0300

    added functionality to checkbox

--- gui/cjs_restaurant/src/main/java/com/cjs_restaurant/MenuScreenController.java
+++ gui/cjs_restaurant/src/main/java/com/cjs_restaurant/MenuScreenController.java
@@ -37,4 +37,68 @@ public class MenuScreenController {
         nextScreenUSingBtnCtrl(btnHome, "home_screen");
     }
 
+    Double totalCost = 0.0,
+    // Appetizers
+    priceChickenWings = 350.0, priceMozzarella = 300.0, priceGarlicBread = 250.0,
+    // Entrees
+    priceGrilledSalmon = 800.0, priceSpaghettiBolognese = 600.0, priceBeefStirFry = 750.0,
+    // Desserts
+    priceCheesecake = 350.0, priceChocolateMousse = 300.0, priceIceCreamSundae = 250.0;
+
+    // check event
+
+    /**
+     *
+     * @param checkBoxCtrl is used to determine if the food item is selected.
+     *                     if true, add food item price to the total cost
+     *                     if false, if cost is not zero, remove food item price.
+     * @param menuItemPrice is added or removed from the total cost.
+     */
+    public void checkingEvent(CheckBox checkBoxCtrl, Double menuItemPrice) {
+        if (checkBoxCtrl.isSelected()) {
+            totalCost = totalCost + menuItemPrice;
+            txtTotalCost.setText(String.valueOf(totalCost));
+        } else {
+            if (totalCost != 0.0) {
+                totalCost = totalCost - menuItemPrice;
+                txtTotalCost.setText(String.valueOf(totalCost));
+            }
+        }
+    }
+
+    public void actionChickenWings() {
+        checkingEvent(checkChickenWings, priceChickenWings);
+    }
+
+    public void actionMozzarella() {
+        checkingEvent(checkMozzarella, priceMozzarella);
+    }
+
+    public void actionGarlicBread() {
+        checkingEvent(checkGarlicBread, priceGarlicBread);
+    }
+
+    public void actionGrilledSalmon() {
+        checkingEvent(checkGrilledSalmon, priceGrilledSalmon);
+    }
+
+    public void actionSpaghettiBolognese() {
+        checkingEvent(checkSpaghettiBolognese, priceSpaghettiBolognese);
+    }
+
+    public void actionBeefStirFry() {
+        checkingEvent(checkBeefStirFry, priceBeefStirFry);
+    }
+
+    public void actionCheesecake() {
+        checkingEvent(checkCheesecake, priceCheesecake);
+    }
+
+    public void actionChocolateMousse() {
+        checkingEvent(checkChocolateMousse, priceChocolateMousse);
+    }
+
+    public void actionIceCreamSundae() {
+        checkingEvent(checkIceCreamSundae, priceIceCreamSundae);
+    }
 }
